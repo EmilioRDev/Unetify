26a65073c120821af7bf0987a7bb95aeadde2bae
Zakri318 <joseemilio0501@gmail.com>
2023-05-04 18:44:56 +0200

    Creación de la clase RegisterActivity, su xml e intent para poder moverse a la nueva actividad

--- app/src/main/java/com/example/unetify/MainActivity.java
+++ app/src/main/java/com/example/unetify/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.unetify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mtvRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mtvRegister = findViewById(R.id.tvRegister);
+        mtvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
