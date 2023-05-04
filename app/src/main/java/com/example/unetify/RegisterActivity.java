9153c3d95438db0f97fa880c2ee91b5c39576920
Zakri318 <joseemilio0501@gmail.com>
2023-05-04 19:48:35 +0200

    Añadido boton para ir atras en el RegisterActivity

--- app/src/main/java/com/example/unetify/RegisterActivity.java
+++ app/src/main/java/com/example/unetify/RegisterActivity.java
@@ -2,13 +2,27 @@ package com.example.unetify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    CircleImageView mCircleImageViewBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mCircleImageViewBack = findViewById(R.id.circleImageBack);
+        mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
