ee241ba8e160e4ed0ffad324ba2705e40430e7ae
Zakri318 <joseemilio0501@gmail.com>
2023-05-06 21:47:10 +0200

    Creado los fragments para el home

--- app/src/main/java/com/example/unetify/activities/HomeActivity.java
+++ app/src/main/java/com/example/unetify/activities/HomeActivity.java
@@ -1,16 +1,58 @@
 package com.example.unetify.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.unetify.R;
+import com.example.unetify.fragments.ChatsFragment;
+import com.example.unetify.fragments.FiltersFragment;
+import com.example.unetify.fragments.HomeFragment;
+import com.example.unetify.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        openFragment(new HomeFragment());
+        bottomNavigation.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.itemHome:
+                        openFragment(new HomeFragment());
+                        return true;
+                    case R.id.itemChats:
+                        openFragment(new ChatsFragment());
+                        return true;
+                    case R.id.itemProfile:
+                        openFragment(new ProfileFragment());
+                        return true;
+                    case R.id.itemFilters:
+                        openFragment(new FiltersFragment());
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
