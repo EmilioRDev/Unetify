0002b6ac53a1d1f59bc431a71dd6815d1c31bf89
Zakri318 <joseemilio0501@gmail.com>
2023-05-06 11:53:50 +0200

    Control de los campos introducidos en login y register

--- app/src/main/java/com/example/unetify/MainActivity.java
+++ app/src/main/java/com/example/unetify/MainActivity.java
@@ -4,24 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mtvRegister;
+    TextView mTextViewRegister;
+    TextInputEditText mTextInputEmail, mTextInputPassword;
+    Button mButtonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mtvRegister = findViewById(R.id.tvRegister);
-        mtvRegister.setOnClickListener(new View.OnClickListener() {
+        mTextViewRegister = findViewById(R.id.textViewRegister);
+        mTextInputEmail = findViewById(R.id.TextInputEmail);
+        mTextInputPassword = findViewById(R.id.TextInputPassword);
+        mButtonLogin = findViewById(R.id.btnLogin);
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void login(){
+        String email = mTextInputEmail.getText().toString();
+        String password = mTextInputPassword.getText().toString();
+        Log.d("CAMPO", "email: "+email);
+        Log.d("CAMPO", "password: "+password);
     }
 }
--- app/src/main/java/com/example/unetify/RegisterActivity.java
+++ app/src/main/java/com/example/unetify/RegisterActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
 
     CircleImageView mCircleImageViewBack;
+    TextInputEditText mtextInputUsername, mtextInputEmail, mtextInputPassword, mtextInputConfirmPassword;
+    Button mButtonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +27,47 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mCircleImageViewBack = findViewById(R.id.circleImageBack);
+        mtextInputUsername = findViewById(R.id.textInputUsername);
+        mtextInputEmail = findViewById(R.id.textInputEmail);
+        mtextInputPassword = findViewById(R.id.textInputPassword);
+        mtextInputConfirmPassword = findViewById(R.id.textInputConfirmPassword);
+        mButtonRegister = findViewById(R.id.btnRegister);
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
         mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+    }
+
+    private void register(){
+        String username = mtextInputUsername.getText().toString();
+        String email = mtextInputEmail.getText().toString();
+        String password = mtextInputPassword.getText().toString();
+        String confirmPassword = mtextInputConfirmPassword.getText().toString();
+
+        if (!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmPassword.isEmpty()) {
+            if (isEmailValid(email)) {
+                Toast.makeText(this, "Insertastes todos los campos y el email es válido", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "Insertastes todos los campos y el email no es válido", Toast.LENGTH_LONG).show();
+            }
+        }else{
+            Toast.makeText(this, "Para continuar inserta todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
